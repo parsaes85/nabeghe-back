80fa2c8e691fcd4e9214c765b5316e6c378e7758
parsa <parsaes771385@gmail.com>
2025-02-16 20:10:44 +0330

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+    console.log("JSON Server is running")
+});
